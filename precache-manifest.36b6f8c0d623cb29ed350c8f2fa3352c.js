6a8a6c328a0913bb12a2db20ad30d59bcd189d76
kerkoub <kerkoub.abdelghani@gmail.com>
2020-04-08 10:07:14 +0200

    Updates

---
+++ precache-manifest.36b6f8c0d623cb29ed350c8f2fa3352c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54136fdf439817f1d19dc26a1d7ad35b",
+    "url": "/allocine/index.html"
+  },
+  {
+    "revision": "e872249bbc1a5ba2e62e",
+    "url": "/allocine/static/css/main.19a4b5f2.chunk.css"
+  },
+  {
+    "revision": "8aef3a143bc7b9bdd178",
+    "url": "/allocine/static/js/2.a57e71dd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/allocine/static/js/2.a57e71dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e872249bbc1a5ba2e62e",
+    "url": "/allocine/static/js/main.8eb5758b.chunk.js"
+  },
+  {
+    "revision": "c3bbf99bf1bff52c76ab",
+    "url": "/allocine/static/js/runtime-main.fd793dcc.js"
+  },
+  {
+    "revision": "b1e2a6296744b505a12a630f2ad8c7d9",
+    "url": "/allocine/static/media/contagionImg.b1e2a629.jpg"
+  }
+]);
